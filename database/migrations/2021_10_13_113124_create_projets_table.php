df391ff9a74d1d5cd1448ee69c93b2a627e7f1da
SARA <sara.polese.9092@gmail.com>
2021-10-13 14:16:17 +0200

    first table
    add fields table projet and add FK

--- database/migrations/2021_10_13_113124_create_projets_table.php
+++ database/migrations/2021_10_13_113124_create_projets_table.php
@@ -14,8 +14,29 @@ class CreateProjetsTable extends Migration
     public function up()
     {
         Schema::create('projets', function (Blueprint $table) {
+            // je crée mes champs
             $table->id();
             $table->timestamps();
+            $table->char('nom', 30);
+            $table->date('date_debut');
+            $table->date('date_fin');
+            $table->string('etape');
+            
+            // je crée mes relation
+            $table->unsignedBigInteger('document_id');
+            $table->foreign('document_id')
+                    ->references('id')
+                    ->on('documents')
+                    ->onDelete('restrict')
+                    ->onUpdate('restrict');
+
+            $table->unsignedBigInteger('personne_id');
+            $table->foreign('personne_id')
+                    ->references('id')
+                    ->on('personnes')
+                    ->onDelete('restrict')
+                    ->onUpdate('restrict');
+            
         });
     }
 
